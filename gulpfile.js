b598cf41dc420ff5e1c40e640486ac0c8d363639
王鸿源honingwon <honingwon@gmail.com>
2017-08-15 10:05:02 +0800

    demo completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
+var path = require('path');
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
+var childProcess = require('child_process');
+
+var actionScriptProperties = path.join(__dirname, '.actionScriptProperties');
+var argStr = `${actionScriptProperties};iflash=false;windowshow=false;chromerun=false;quickcompile=true`;
+var layajs = path.join(__dirname, './.laya/astool/layajs');
 
 gulp.task('livereload', [], function () {
     livereload({ start: true });
-
     gulp.watch('./bin/**/*.js', (file) => {
         livereload.changed(file.path);
     });
@@ -11,12 +16,22 @@ gulp.task('livereload', [], function () {
 
 gulp.task('compile', [], function () {
     gulp.watch('./src/**/*.as', (file) => {
-        // console.log(file);
-        // .laya/astool/layajs
-        // .actionScriptProperties;iflash=false;windowshow=false;chromerun=false;quickcompile=true
+        console.log(file.path);
+        var child = childProcess.spawn(layajs, [argStr]);
+        var t1 = new Date().getTime();
+        console.log('compile start...');
+        child.stdout.on('data', function (chunk) {
+            console.log(chunk + '')
+        });
+        child.stderr.on('data', function (chunk) {
+            console.log('error', chunk)
+        });
+        child.on('exit', function (code, signal) {
+            var t2 = new Date().getTime();
+            console.log(`compile end within ${t2 - t1} ms`);
+        });
     });
 });
 
 gulp.task('default', ['livereload', 'compile'], function () {
-
 });
